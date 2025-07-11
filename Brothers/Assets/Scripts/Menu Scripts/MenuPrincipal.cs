63482336efb38f73c3ef41b7f544e862884ee634
Miguel Santos Oliveira <miguel.santos321_@hotmail.com>
2025-07-11 11:25:47 -0300

    Adding more buttons

--- Brothers/Assets/Scripts/Menu Scripts/MenuPrincipal.cs
+++ Brothers/Assets/Scripts/Menu Scripts/MenuPrincipal.cs
@@ -12,16 +12,14 @@ public class MenuPrincipal : MonoBehaviour
     {
         TextoMenu.gameObject.LeanScale(new Vector3(1.3f,1.3f),0.45f).setLoopPingPong();
     }
-    private void Update()
+    
+    public void PannelIN()
     {
-        if (Input.GetKeyDown(KeyCode.E)) 
-        {
-                Pannel_Levels.LeanMoveX(30f, 1f).setEaseInBounce();
-                Debug.Log("VC APERTOU!");   
-        }
-        /*else if(Input.GetKeyDown(KeyCode.Escape))  
-        {
-            Pannel_Levels.LeanMoveX(820f, 0.3f);
-        }*/
+        Pannel_Levels.LeanMoveX(30f, 1f);
+    }
+
+    public void PannelOUT() 
+    {
+        Pannel_Levels.LeanMoveX(820f, 0.7f);
     }
 }
