270dcd12b5e11725fa7ab67b1c12ee26da9693ae
Miguel Santos Oliveira <miguel.santos321_@hotmail.com>
2025-09-16 10:56:58 -0300

    Prototipando Minigame números
    Começando os rascunhos do jogo de numeros

---
+++ Brothers/Assets/Scripts/Game Scripts/Numeros-Scripts/Number_Manager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Number_Manager : MonoBehaviour
+{
+    [SerializeField]
+    private int Conter;
+    public GameObject Numeros;
+
+    void Start()
+    {
+        Conter = 0;
+    }
+    public void instanciar()
+    {
+
+    }
+
+}
