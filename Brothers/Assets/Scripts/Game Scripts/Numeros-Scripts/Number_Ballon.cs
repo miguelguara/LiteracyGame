719cd6d9a1e75d693a9f7600a64bbed50284de43
Miguel Santos Oliveira <miguel.santos321_@hotmail.com>
2025-10-04 00:04:14 -0300

    Adding the ballon animations

--- Brothers/Assets/Scripts/Game Scripts/Numeros-Scripts/Number_Ballon.cs
+++ Brothers/Assets/Scripts/Game Scripts/Numeros-Scripts/Number_Ballon.cs
@@ -2,21 +2,30 @@ using UnityEngine;
 
 public class Number_Ballon : MonoBehaviour
 {
+    //O nimero que será comparado como uma TAG
     [SerializeField]
     private int numero;
-
+    [SerializeField]
+    private AudioClip popClip;
     private Number_Manager NM;
+    private AudioControl AC;
+    private Animator anim;
     void Start()
     {
         NM = FindFirstObjectByType<Number_Manager>();
+        anim = GetComponent<Animator>();
+        AC = Object.FindFirstObjectByType<AudioControl>();
     }
 
     void OnCollisionEnter2D(Collision2D c)
     {
         if (c.gameObject.GetComponent<Number>().numero == numero)
         {
-           // c.gameObject.GetComponent<Number>().som_Numero();
-            NM.instanciar();    
+            // c.gameObject.GetComponent<Number>().som_Numero();
+            anim.SetBool("Pop", true);
+            NM.instanciar();
+            AC.Tocar_SFX(popClip);
+            Destroy(this.gameObject, 0.3f);
         }
     }
 }
