4655ade286df287a4fa62ec60f53caee552a70f4
Miguel Santos Oliveira <miguel.santos321_@hotmail.com>
2025-10-30 00:37:55 -0300

    Update Number_Manager.cs

--- Brothers/Assets/Scripts/Game Scripts/Numeros-Scripts/Number_Manager.cs
+++ Brothers/Assets/Scripts/Game Scripts/Numeros-Scripts/Number_Manager.cs
@@ -9,7 +9,7 @@ public class Number_Manager : MonoBehaviour
     //Isso vai limitar quantas vezes irá espawnar os numeros!!!!
     [SerializeField]
     private int limit;
-    public GameObject Numeros;
+    public GameObject [] Numeros;
     [SerializeField]
     RectTransform Vitoria_Panel;
 
@@ -23,11 +23,12 @@ public class Number_Manager : MonoBehaviour
     }
     public void instanciar()
     {
-        if (Conter < limit)
+        if (Conter <= limit)
         {
             float x = Random.Range(-9f, 9f);
             float y = Random.Range(-4f, 3f);
-            Instantiate(Numeros, new Vector2(x, y), Quaternion.identity);
+            int n = Random.Range(Num_Ini, Num_Fin);
+            Instantiate(Numeros[n], new Vector2(x, y), Quaternion.identity);
             AtualizarUI();
         }
         else
