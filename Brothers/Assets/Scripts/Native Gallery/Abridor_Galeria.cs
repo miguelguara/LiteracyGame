a322cc98d8fbad0cd214f0b8b830e5542ea53861
Miguel Santos Oliveira <miguel.santos321_@hotmail.com>
2025-12-06 23:10:23 -0300

    Atualizações!!

--- Brothers/Assets/Scripts/Native Gallery/Abridor_Galeria.cs
+++ Brothers/Assets/Scripts/Native Gallery/Abridor_Galeria.cs
@@ -1,13 +1,59 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Abridor_Galeria : MonoBehaviour
 {
  [SerializeField]
  private Image image;
+ private Transform imageTransform;
+ [SerializeField]
+ private Texture2D tex;
 
-//Função que chama a Bibliotéca Native Gallery
- public void AbrirGaleria()
+ [SerializeField]
+ Button abrirGaleriaButton;
+ private static Abridor_Galeria instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnsceneLoaded;
+    }
+    //Quando a cena de criar fase iniciar checar se o ha alguma textura presente
+    public void OnsceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if(scene.name == "Tela_Inicial")
+        {
+          //Acha o botão e atribui a função de abrir a galeria
+          abrirGaleriaButton = GameObject.Find("GaleryButton").GetComponent<Button>();
+          abrirGaleriaButton.onClick.AddListener(AbrirGaleria);
+          if(tex != null)
+            {
+         imageTransform = GameObject.Find("ImageBG").transform;     
+         image = imageTransform.Find("Word_Image").GetComponent<Image>();
+         image.gameObject.SetActive(true);
+         //Cria um sprite a partir da textura carregada e atribui para a imagem da UI
+         Sprite sp = Sprite.Create(tex, new Rect(0f,0f,tex.width,tex.height), new Vector2(0.5f,0.5f));
+         image.sprite = sp;  
+            }
+        }
+    }
+    
+
+    //Função que chama a Bibliotéca Native Gallery
+    public void AbrirGaleria()
     {
         NativeGallery.GetImageFromGallery((path) =>
         {
@@ -15,11 +61,12 @@ public class Abridor_Galeria : MonoBehaviour
         if(path != null)
             {
               //Carrega uma textura a partir da imagem selecionada
-                Texture2D tex = NativeGallery.LoadImageAtPath(path);
+               tex = NativeGallery.LoadImageAtPath(path);
                 if(tex != null)
             {
               //Cria um sprite a partir da textura carregada e atribui para a imagem da UI
                 Sprite sp =Sprite.Create(tex, new Rect(0f,0f,tex.width,tex.height), new Vector2(0.5f,0.5f));
+                image.gameObject.SetActive(true);
                 image.sprite = sp;
             }
             }    
